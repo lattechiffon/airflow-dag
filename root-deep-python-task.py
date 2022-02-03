c83e5082d55df73eb342281f90fcc674ad7ff0fe
Yong-Guk Go <lattechiffon@gmail.com>
2022-02-03 11:38:32 +0900

    Update root-deep-python-task.py

--- root-deep-python-task.py
+++ root-deep-python-task.py
@@ -34,5 +34,47 @@ print_goodbye = PythonOperator(
 	dag = dag
     )
 
+print_hello1 = PythonOperator(
+	task_id = 'print_hello1',
+	#python_callable param points to the function you want to run 
+	python_callable = print_hello,
+	#dag param points to the DAG that this task is a part of
+	dag = dag
+    )
+
+print_goodbye1 = PythonOperator(
+	task_id = 'print_goodbye1',
+	python_callable = print_goodbye,
+	dag = dag
+    )
+
+print_hello2 = PythonOperator(
+	task_id = 'print_hello2',
+	#python_callable param points to the function you want to run 
+	python_callable = print_hello,
+	#dag param points to the DAG that this task is a part of
+	dag = dag
+    )
+
+print_goodbye2 = PythonOperator(
+	task_id = 'print_goodbye2',
+	python_callable = print_goodbye,
+	dag = dag
+    )
+
+print_hello3 = PythonOperator(
+	task_id = 'print_hello3',
+	#python_callable param points to the function you want to run 
+	python_callable = print_hello,
+	#dag param points to the DAG that this task is a part of
+	dag = dag
+    )
+
+print_goodbye3 = PythonOperator(
+	task_id = 'print_goodbye3',
+	python_callable = print_goodbye,
+	dag = dag
+    )
+
 #Assign the order of the tasks in our DAG
-print_hello >> print_goodbye >> print_hello >> print_goodbye >> print_hello >> print_goodbye >> print_hello >> print_goodbye >> print_hello >> print_goodbye >> print_hello >> print_goodbye >> print_hello >> print_goodbye >> print_hello >> print_goodbye >> print_hello >> print_goodbye >> print_hello >> print_goodbye
+print_hello >> print_goodbye >> print_hello1 >> print_goodbye1 >> print_hello2 >> print_goodbye2 >> print_hello3 >> print_goodbye3
